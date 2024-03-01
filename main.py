7ddd21be485c4e506175f659fcccd209e4f6e51a
Ho Nyeong <dev.honing@gmail.com>
2024-03-01 23:24:42 +0900

    Feat: 배열의 요소 계산하기

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main():
     print("\n2. 배열의 크기: ")
     print(array_2d.shape) # 배열의 크기 출력
 
-    # 배열의 요소에 접근하기
+    # 배열의 요소에 접근
     print("\n3. 배열의 요소에 접근: ")
     print("<행 출력>")
     print("첫번째 행: ", array_2d[0])
@@ -21,6 +21,16 @@ def main():
     print("첫번째 열: ", array_2d[:, 0])
     print("두번째 열: ", array_2d[:, 1])
     print("세번째 열: ", array_2d[:, 2])
+
+    # 배열의 요소의 합, 평균, 최댓값, 최솟값, 표준 편차 계산
+    print("\n4. 배열의 통계적 특성: ")
+    print("요소의 합: ", np.sum(array_2d))
+    print("평균 값: ", np.mean(array_2d))
+    print("최댓값: ", np.max(array_2d))
+    print("최솟값: ", np.min(array_2d))
+    print("표준 편차: ", np.std(array_2d))
+
+
 
 if __name__ == "__main__":
     main()
